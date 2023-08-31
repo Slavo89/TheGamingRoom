709af78f681cbac1a1f40cd3a23122384ecefe2a
Sławomir Żagan <szaagan@gmail.com>
2023-08-31 16:53:49 +0200

    final version

--- src/data/iconsSRC.jsx
+++ src/data/iconsSRC.jsx
@@ -1,19 +1,19 @@
 export const ratings = {
-	everyone: '/src/assets/ratingLogos/E.svg',
-	'everyone-10-plus': '/src/assets/ratingLogos/E10plus.svg',
-	teen: '/src/assets/ratingLogos/T.svg',
-	mature: '/src/assets/ratingLogos/M.svg',
-	adult: '/src/assets/ratingLogos/AO.svg',
+	everyone: '/assets/ratingLogos/E.svg',
+	'everyone-10-plus': '/assets/ratingLogos/E10plus.svg',
+	teen: '/assets/ratingLogos/T.svg',
+	mature: '/assets/ratingLogos/M.svg',
+	adult: '/assets/ratingLogos/AO.svg',
 };
 
 export const platforms = {
-	PC: '/src/assets/platformIcons/windows.png',
-	PlayStation: '/src/assets/platformIcons/playstation.png',
-	Xbox: '/src/assets/platformIcons/xbox.png',
-	Nintendo: '/src/assets/platformIcons/nintendo.png',
-	'Apple Macintosh': '/src/assets/platformIcons/apple.png',
-	Linux: '/src/assets/platformIcons/linux.png',
-	Android: '/src/assets/platformIcons/android.png',
-	iOS: '/src/assets/platformIcons/apple.png',
-	Web: '/src/assets/platformIcons/web.png',
+	PC: '/assets/platformIcons/windows.png',
+	PlayStation: '/assets/platformIcons/playstation.png',
+	Xbox: '/assets/platformIcons/xbox.png',
+	Nintendo: '/assets/platformIcons/nintendo.png',
+	'Apple Macintosh': '/assets/platformIcons/apple.png',
+	Linux: '/assets/platformIcons/linux.png',
+	Android: '/assets/platformIcons/android.png',
+	iOS: '/assets/platformIcons/apple.png',
+	Web: '/assets/platformIcons/web.png',
 };
